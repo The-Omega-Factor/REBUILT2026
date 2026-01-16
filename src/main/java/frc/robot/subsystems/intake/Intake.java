d080645aea4fd42a77c46d8b509846fc6e4aa18a
Hao Le <hao.levoquoc2010@gmail.com>
2026-01-16 16:11:57 -0600

    Recommitting last commit
    Some changes did not get staged

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems.intake;
 
-public class Intake {
-    
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+public class Intake extends SubsystemBase {
+    private static final Intake intakeSystem = new Intake();
+    private final TalonFX intake;
+
+    private Intake() {
+        intake = new TalonFX(IntakeConstants.intakeID, new CANBus(IntakeConstants.intakeCanName));
+    }
+
+    public void setSpeed(double speed) {
+        intake.set(speed);
+    }
+
+    public double getIntakePosition() {
+        return intake.getPosition().getValueAsDouble();
+    }
 }
