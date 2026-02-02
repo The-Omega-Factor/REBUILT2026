5828e16115154237bd9e94baf09585b53e0bb3f5
Hao Le <hao.levoquoc2010@gmail.com>
2026-02-02 15:57:32 -0600

    Recommit

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.shooter;
 
-import java.util.function.DoubleSupplier;
-
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
