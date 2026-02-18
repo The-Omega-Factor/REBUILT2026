74f2c4a5f8986c03174e5db55712f8830000ff5a
Alpha star <omegafactorrobotics@gmail.com>
2026-02-18 15:33:23 -0600

    Added indexer logic

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
@@ -12,18 +16,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
     private final TalonFXConfiguration shooterLConfig = new TalonFXConfiguration();
-    private final TalonFX shooterL = new TalonFX(0, "SwerveBase");
-    private final TalonFX shooterR = new TalonFX(14, "SwerveBase");
+    private final CANBus canbus = new CANBus("SwerveBase");
+    private final TalonFX shooterL = new TalonFX(0, canbus);
+    private final TalonFX shooterR = new TalonFX(14, canbus);
 
-    private final VelocityVoltage request = new VelocityVoltage(0);
+    private final VelocityVoltage request = new VelocityVoltage(0).withSlot(0);
+
+    private final TalonFXConfiguration transferConfig = new TalonFXConfiguration();
+    private TalonFX transfer = new TalonFX(5, canbus);
 
     public ShooterSubsystem() {
         shooterLConfig.Slot0.kP = 0.5;
         shooterLConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
+        shooterLConfig.withCurrentLimits(new CurrentLimitsConfigs()
+        .withStatorCurrentLimit(Amps.of(20))
+        .withStatorCurrentLimitEnable(true));
+
         shooterL.getConfigurator().apply(shooterLConfig);
-        //shooterR.setControl(new Follower(0, MotorAlignmentValue.Aligned));
+        shooterR.setControl(new Follower(shooterL.getDeviceID(), MotorAlignmentValue.Opposed));
         shooterR.setNeutralMode(NeutralModeValue.Coast);
+
+        transferConfig.Slot0.kP = 0.5;
+        transferConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        
     }
 
     public void setVelocity(double speed) {
