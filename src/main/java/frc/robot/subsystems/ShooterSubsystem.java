091edd0d84a0fed35a7f1e62bccde3de979e228b
Hao Le <hao.levoquoc2010@gmail.com>
2026-02-19 16:44:36 -0600

    Implemented constants files values

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
 
-import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
@@ -13,48 +12,47 @@ import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private final CANBus canbus = new CANBus("SwerveBase");
 
     private final TalonFXConfiguration shooterLConfig = new TalonFXConfiguration();
     private final TalonFXConfiguration shooterRConfig = new TalonFXConfiguration();
-    private final TalonFX shooterL = new TalonFX(0, canbus);
-    private final TalonFX shooterR = new TalonFX(14, canbus);
-
+    private final TalonFX shooterL = new TalonFX(Constants.ShooterConstants.shooterLID, Constants.canbus);
+    private final TalonFX shooterR = new TalonFX(Constants.ShooterConstants.shooterRID, Constants.canbus);
 
     private final TalonFXConfiguration hopperConfig = new TalonFXConfiguration();
-    private final TalonFX hopper = new TalonFX(5, canbus);
+    private final TalonFX hopper = new TalonFX(5, Constants.canbus);
 
     private final VelocityVoltage shooterRequest = new VelocityVoltage(0).withSlot(0);
     private final VelocityVoltage hopperRequest = new VelocityVoltage(0).withSlot(0);
 
     public ShooterSubsystem() {
-        shooterLConfig.Slot0.kP = 0.5;
+        shooterLConfig.Slot0.kP = Constants.ShooterConstants.shooterPIDs.kP;
         shooterLConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        shooterLConfig.Feedback.SensorToMechanismRatio = 25/20;
+        shooterLConfig.Feedback.SensorToMechanismRatio = Constants.ShooterConstants.shootersGearRatio;
 
         shooterLConfig.withCurrentLimits(new CurrentLimitsConfigs()
-        .withStatorCurrentLimit(Amps.of(67))
+        .withStatorCurrentLimit(Amps.of(Constants.ShooterConstants.shootersAmpsLimit))
         .withStatorCurrentLimitEnable(true));
 
         shooterL.getConfigurator().apply(shooterLConfig);
         shooterL.setNeutralMode(NeutralModeValue.Coast);
 
         shooterRConfig.withCurrentLimits(new CurrentLimitsConfigs()
-        .withStatorCurrentLimit(Amps.of(67))
+        .withStatorCurrentLimit(Amps.of(Constants.ShooterConstants.shootersAmpsLimit))
         .withStatorCurrentLimitEnable(true));
 
         shooterR.getConfigurator().apply(shooterRConfig);
         shooterR.setNeutralMode(NeutralModeValue.Coast);
         shooterR.setControl(new Follower(shooterL.getDeviceID(), MotorAlignmentValue.Opposed));
 
-        hopperConfig.Slot0.kP = 0.5;
+        hopperConfig.Slot0.kP = Constants.ShooterConstants.hopperPIDs.kP;
         hopperConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        hopperConfig.Feedback.SensorToMechanismRatio = 20/35;
+        hopperConfig.Feedback.SensorToMechanismRatio = Constants.ShooterConstants.hopperGearRatio;
 
         hopperConfig.withCurrentLimits(new CurrentLimitsConfigs()
-        .withStatorCurrentLimit(Amps.of(67))
+        .withStatorCurrentLimit(Amps.of(Constants.ShooterConstants.hopperAmpsLimit))
         .withStatorCurrentLimitEnable(true));
 
         hopper.getConfigurator().apply(hopperConfig);
