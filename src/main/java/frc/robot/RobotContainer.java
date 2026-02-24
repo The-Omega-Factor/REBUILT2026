fdec2320286f24ac668e6fdfe355a0bbc31bcd99
Hao Le <hao.levoquoc2010@gmail.com>
2026-02-24 15:56:22 -0600

    Added first B1 Autonomous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -51,9 +50,10 @@ public class RobotContainer {
     private final IntakeSubsystem intake = new IntakeSubsystem();
 
     public RobotContainer() {
-        NamedCommands.registerCommand("SetIntakeState", new SetIntakeState(intake, null, null));
-        NamedCommands.registerCommand("ShootAndHopper", new ShootAndHopper(shooter, null, null));
-
+        NamedCommands.registerCommand("00LowerIntake", new SetIntakeState(intake, null, null));
+        NamedCommands.registerCommand("01Shoot", new ShootAndHopper(shooter, null, null));
+        NamedCommands.registerCommand("02RaiseIntake", new ShootAndHopper(shooter, null, null));
+        
         autoChooser = AutoBuilder.buildAutoChooser();
 
         configureBindings();
@@ -119,22 +119,7 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        // Simple drive forward auton
-        final var idle = new SwerveRequest.Idle();
-        return Commands.sequence(
-            // Reset our field centric heading to match the robot
-            // facing away from our alliance station wall (0 deg).
-            drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
-            // Then slowly drive forward (away from us) for 5 seconds.
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(0.5)
-                    .withVelocityY(0)
-                    .withRotationalRate(0)
-            )
-            .withTimeout(5.0),
-            // Finally idle for the rest of auton
-            drivetrain.applyRequest(() -> idle)
-        );
+        return autoChooser.getSelected();
     }
 
     public void logPivotPosition() {
