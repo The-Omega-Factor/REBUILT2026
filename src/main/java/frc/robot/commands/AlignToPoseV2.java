98fdad607c036bf42148d3747b5f5299a69a4f38
Hao Le <hao.levoquoc2010@gmail.com>
2026-02-26 21:36:25 -0600

    Finished AlignToPoseV2

---
+++ src/main/java/frc/robot/commands/AlignToPoseV2.java
@@ -0,0 +1,75 @@
+package frc.robot.commands;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class AlignToPoseV2 extends Command {
+    public CommandSwerveDrivetrain drivetrain;
+    public double targetX;
+    public double targetY;
+
+    private final SwerveRequest.FieldCentric driveRequest = new SwerveRequest.FieldCentric();
+    private final PIDController thetaController = new PIDController(4.0, 0.0, 0.0);
+    private double targetHeading;
+
+    private Pose2d currentPose;
+    private double currentX;
+    private double currentY;
+    private double currentHeading;
+
+    private double error;
+
+    public AlignToPoseV2(CommandSwerveDrivetrain drivetrain, double targetX, double targetY) {
+        this.drivetrain = drivetrain;
+        this.targetX = targetX;
+        this.targetY = targetY;
+
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        thetaController.reset();
+        thetaController.setTolerance(Math.toRadians(5));
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    }
+
+    @Override
+    public void execute() {
+        currentPose = drivetrain.getPose();
+
+        currentX = currentPose.getX();
+        currentY = currentPose.getY();
+        currentHeading = MathUtil.angleModulus(currentPose.getRotation().getRadians());
+
+        targetHeading = Math.atan2(targetY - currentY, targetX - currentX);
+        error = MathUtil.clamp(
+            thetaController.calculate(currentHeading, targetHeading),
+            -Constants.Swerve.maxOmega,
+            Constants.Swerve.maxOmega
+            );
+
+        drivetrain.setControl(driveRequest.withRotationalRate(error));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return thetaController.atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.setControl(driveRequest.withRotationalRate(0));
+    }
+
+    public void setTarget(double x, double y) {
+        this.targetX = x;
+        this.targetY = y;
+    }
+}
